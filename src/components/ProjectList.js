0b99b30b7605f06658287f4a15a63224523a2ce1
Sadık Barış Yılmaz <sadikbarisyilmaz@gmail.com>
2023-06-10 12:05:44 +0300

    completed assignment

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -3,10 +3,25 @@ import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
   console.log(projects);
+
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+        {/* <ProjectItem /> */}
+        {/* render ProjectItem components here */}
+        {projects.map((item, i) => {
+          console.log(item);
+          return (
+            <ProjectItem
+              key={i}
+              name={item.name}
+              about={item.about}
+              technologies={item.technologies}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
